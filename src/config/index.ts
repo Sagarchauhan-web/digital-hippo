e63c332bc8544d209c22e57f5869100106d64430
Sagar <Sagarchawan4646@gmail.com>
2024-03-07 01:58:36 +0530

    Nav items

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,9 +3,9 @@ export const PRODUCT_CATEGORIES = [
     label: 'UI Kits',
     value: 'ui_kits' as const,
     featured: [
-      { name: 'Editor picks', href: '#', imageSrc: '/nav/ui-kits/mixed.jgp' },
-      { name: 'New arrivals', href: '#', imageSrc: '/nav/ui-kits/blue.jgp' },
-      { name: 'Best sellers', href: '#', imageSrc: '/nav/ui-kits/purple.jgp' },
+      { name: 'Editor picks', href: '#', imageSrc: '/nav/ui-kits/mixed.jpg' },
+      { name: 'New arrivals', href: '#', imageSrc: '/nav/ui-kits/blue.jpg' },
+      { name: 'Best sellers', href: '#', imageSrc: '/nav/ui-kits/purple.jpg' },
     ],
   },
   {
@@ -15,13 +15,13 @@ export const PRODUCT_CATEGORIES = [
       {
         name: 'Favorite icon picks',
         href: '#',
-        imageSrc: '/nav/icons/picks.jgp',
+        imageSrc: '/nav/icons/picks.jpg',
       },
-      { name: 'New arrivals', href: '#', imageSrc: '/nav/icons/new.jgp' },
+      { name: 'New arrivals', href: '#', imageSrc: '/nav/icons/new.jpg' },
       {
         name: 'Best sellers icons',
         href: '#',
-        imageSrc: '/nav/icons/bestsellers.jgp',
+        imageSrc: '/nav/icons/bestsellers.jpg',
       },
     ],
   },
