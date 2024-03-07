7c9682f5819b00f64316dfc27043f1b29846312f
Sagar <Sagarchawan4646@gmail.com>
2024-03-07 18:04:30 +0530

    Navbar styles

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,33 @@
 'use client';
 
 import { PRODUCT_CATEGORIES } from '@/config';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
   const isAnyOpen = activeIndex !== null;
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener('keydown', (e) => handler(e));
+
+    return () => document.removeEventListener('keydown', (e) => handler(e));
+  }, []);
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className='flex gap-4 h-full'>
+    <div className='flex gap-4 h-full' ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
